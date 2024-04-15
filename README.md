20e41d017daab4e907f9355982a72e156152aa7f
Haftamu Kebede <54979169+haftamuk@users.noreply.github.com>
2024-04-15 19:51:55 +0300

    GT06 - UM552 - 6006

---
+++ README.md
@@ -0,0 +1,8 @@
+Protocol/Model : GT06
+
+Running the app: node index.js
+
+Protocols list
+https://www.traccar.org/protocols/
+
+https://github.com/traccar/traccar/issues/1445
---
+++ index.js
@@ -0,0 +1,24 @@
+var gps = require("gps-tracking");
+
+var options = {
+  debug: true,
+  port: 6006,
+  device_adapter: "GT06",
+};
+
+var server = gps.server(options, function (device, connection) {
+  device.on("login_request", function (device_id, msg_parts) {
+    // Some devices sends a login request before transmitting their position
+    // Do some stuff before authenticate the device...
+
+    // Accept the login request. You can set false to reject the device.
+    this.login_authorized(true);
+  });
+
+  //PING -> When the gps sends their position
+  device.on("ping", function (data) {
+    //After the ping is received, but before the data is saved
+    console.log("GPS TRACKER DATA - ", data);
+    return data;
+  });
+});
