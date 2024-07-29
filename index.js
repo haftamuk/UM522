2b2498834b12b349828e06df70341183462d11cf
Haftamu Kebede <54979169+haftamuk@users.noreply.github.com>
2024-07-29 15:34:34 +0300

    CRS data proxied through MooveLocation

--- index.js
+++ index.js
@@ -125,6 +125,7 @@ var server = gps.server(options, function (device, connection) {
 
   //Also, you can listen on the native connection object
   connection.on("data", function (data) {
+    logger.info("Connection Obj: " + JSON.parse(connection.toString()));
 
     let bufferToHexString = function (buffer) {
       var str = '';
