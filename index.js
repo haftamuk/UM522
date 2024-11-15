5f5e07586ac89b9447aaa963e9cc7a3be3df377b
Haftamu Kebede <54979169+haftamuk@users.noreply.github.com>
2024-11-15 18:04:09 +0300

    pino logger disabled as it asks subscription fee

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const token = 'j8AqyzxzWToPcBudDE2xABrd';
-const pino = require('pino');
+const token = "j8AqyzxzWToPcBudDE2xABrd";
+// const pino = require("pino");
 const net = require("net");
 
 var gps = require("gps-tracking");
@@ -10,18 +10,23 @@ var options = {
   device_adapter: "GT06",
 };
 
-
-const transport = pino.transport({
-  target: "@logtail/pino",
-  options: { sourceToken: token }
-});
+// const transport = pino.transport({
+//   target: "@logtail/pino",
+//   options: { sourceToken: token },
+// });
 // Plates: Land cruiser(74740) / UM552, 62940 / um552, 77437 / UM552, 3-B77827 / UM552, A65331/ TK003, 3-16636/ TK003,  B77849 / TK003
-const crsTerminals = ["0358657104462051","0868720061903625", "0868720061906289", "0868720061905174", "0868720061898619", "0358657104517136", "0358657103861956", "0358657104813964"]
+const crsTerminals = [
+  "0358657104462051",
+  "0868720061903625",
+  "0868720061906289",
+  "0868720061905174",
+  "0868720061898619",
+  "0358657104517136",
+  "0358657103861956",
+  "0358657104813964",
+];
 
-const logger = pino(transport);
-
-
-
+// const logger = pino(transport);
 
 var server = gps.server(options, function (device, connection) {
   // #######################################################################################################################
@@ -30,44 +35,46 @@ var server = gps.server(options, function (device, connection) {
   let client = new net.Socket();
   let is_proxy_CRS_device = false;
   try {
-    client.connect(20859, '193.193.165.165', function () {
-      console.log("==========================================================================");
-      console.log('CRS- Connected ');  // acknowledge socket connection
-      console.log("==========================================================================");
+    client.connect(20859, "193.193.165.165", function () {
+      console.log(
+        "=========================================================================="
+      );
+      console.log("CRS- Connected "); // acknowledge socket connection
+      console.log(
+        "=========================================================================="
+      );
 
-      logger.info("CRS - CONNECTED.");
+      console.log("CRS - CONNECTED.");
     });
-    console.log('CRS - DEVICE Connected ');  // acknowledge socket connection
-
+    console.log("CRS - DEVICE Connected "); // acknowledge socket connection
   } catch (error) {
-
-    logger.info("CRS - ERROR : " + error.message);
-    console.log("==========================================================================");
     console.log("CRS - ERROR : " + error.message);
-    console.log("==========================================================================");
-
+    console.log(
+      "=========================================================================="
+    );
+    console.log("CRS - ERROR : " + error.message);
+    console.log(
+      "=========================================================================="
+    );
   }
-
 
   client.on("error", (err) => {
     console.log("CRS - Error Connecting : " + err.message);
-    logger.info("CRS - Error Connecting : " + err.message);
+    console.log("CRS - Error Connecting : " + err.message);
   });
 
-
   function bufferToHexString(buffer) {
-    var str = '';
+    var str = "";
     for (var i = 0; i < buffer.length; i++) {
       if (buffer[i] < 16) {
-        str += '0';
+        str += "0";
       }
       str += buffer[i].toString(16);
     }
 
-
-    console.log("bufferToHexString : ", str)
+    console.log("bufferToHexString : ", str);
     return str;
-  };
+  }
 
   function isProxyCRSDevice(value) {
     for (var i = 0; i < crsTerminals.length; i++) {
@@ -78,42 +85,37 @@ var server = gps.server(options, function (device, connection) {
     return false;
   }
 
-
-
-
   device.on("connected", function () {
-    console.log('DEVICE Connected ');  // acknowledge socket connection
-    logger.info('CRS - I am a new device CONNECTED');
+    console.log("DEVICE Connected "); // acknowledge socket connection
+    console.log("CRS - I am a new device CONNECTED");
   });
 
   device.on("disconnected", function () {
-    logger.info("CRS - Device DISCONNECTED");
-    console.log('DEVICE DISConnected ');  // acknowledge socket connection
-    // client.end(); // kill client after server's response 
+    console.log("CRS - Device DISCONNECTED");
+    console.log("DEVICE DISConnected "); // acknowledge socket connection
+    // client.end(); // kill client after server's response
   });
 
   device.on("login_request", function (device_id, msg_parts) {
     is_proxy_CRS_device = crsTerminals.includes(device_id);
 
-    logger.info("LOGIN_REQUEST EMITTED. My name is " + device_id);
+    console.log("LOGIN_REQUEST EMITTED. My name is " + device_id);
 
     console.log("LOGIN_REQUEST EMITTED. My name is " + device_id);
 
     this.login_authorized(true);
 
-    logger.info("Ok, " + device_id + ", you're accepted!");
-    logger.info("LOGIN REQUEST CONTENT : ");
-    logger.info(JSON.stringify(msg_parts));
-
+    console.log("Ok, " + device_id + ", you're accepted!");
+    console.log("LOGIN REQUEST CONTENT : ");
+    console.log(JSON.stringify(msg_parts));
   });
 
-
   device.on("ping", function (data, msg_parts) {
-    logger.info("PING REQUEST CONTENT MSG_PARTS: ");
-    logger.info(JSON.stringify(msg_parts));
+    console.log("PING REQUEST CONTENT MSG_PARTS: ");
+    console.log(JSON.stringify(msg_parts));
 
-    logger.info("PING REQUEST CONTENT DATA: ");
-    logger.info(JSON.stringify(data));
+    console.log("PING REQUEST CONTENT DATA: ");
+    console.log(JSON.stringify(data));
     /**
      * #######################################################################
      * ########## SENDING LOCATION INFORMATION TO SERVER ######################
@@ -128,13 +130,13 @@ var server = gps.server(options, function (device, connection) {
       },
     })
       .then((response) => response.json())
-      .then((data) => logger.info("MooveLocation Returned Data"));
+      .then((data) => console.log("MooveLocation Returned Data", data));
     /**
      * #######################################################################
      */
 
     //this = device
-    // logger.info(
+    // console.log(
     //   "I'm here: " +
     //     data.latitude +
     //     ", " +
@@ -145,15 +147,15 @@ var server = gps.server(options, function (device, connection) {
     // );
 
     //Look what informations the device sends to you (maybe velocity, gas level, etc)
-    //    logger.info("HERE IS GPS Tracker data sent:", JSON.stringify(data));
+    //    console.log("HERE IS GPS Tracker data sent:", JSON.stringify(data));
     return data;
   });
 
   device.on("alarm", function (alarm_code, alarm_data, msg_data) {
-    logger.info("ALARM REQUEST CONTENT MSG_PARTS: ");
-    logger.info(JSON.stringify(msg_data));
+    console.log("ALARM REQUEST CONTENT MSG_PARTS: ");
+    console.log(JSON.stringify(msg_data));
 
-    logger.info(
+    console.log(
       "Help! Something happend: " + alarm_code + " (" + alarm_data.msg + ")"
     );
 
@@ -172,47 +174,47 @@ var server = gps.server(options, function (device, connection) {
     })
       .then((response) => response.json())
       .then((data) => {
-        logger.info("MooveLocation Returned Result.");
-        // logger.info(data);
-      }
-      );
+        console.log("MooveLocation Returned Result.");
+        // console.log(data);
+      });
     /**
      * #######################################################################
      */
   });
 
-
-
-
-
-
-
-
-
-
-
-
-
   //Also, you can listen on the native connection object
   connection.on("data", function (data) {
-    // logger.info("Connection Obj: " + Object.toString(connection));
+    // console.log("Connection Obj: " + Object.toString(connection));
     if (is_proxy_CRS_device) {
       //echo raw data package
-      console.log("==========================================================================");
+      console.log(
+        "=========================================================================="
+      );
       console.log("CRS - RAW DATA emitted : IMEI - " + bufferToHexString(data));
-      console.log("==========================================================================");
+      console.log(
+        "=========================================================================="
+      );
       client.write(data)
-        ? console.log("CRS - Data Written to CRS server : " + bufferToHexString(data))
-        : console.log("CRS - NOT Written to CRS server : " + bufferToHexString(data));
-      console.log("==========================================================================");
-
+        ? console.log(
+            "CRS - Data Written to CRS server : " + bufferToHexString(data)
+          )
+        : console.log(
+            "CRS - NOT Written to CRS server : " + bufferToHexString(data)
+          );
+      console.log(
+        "=========================================================================="
+      );
     } else {
       //echo raw data package
-      console.log("==========================================================================");
-      logger.info("MOOVE Location - RAW DATA emitted : IMEI - " + bufferToHexString(data));
-      console.log("==========================================================================");
-
+      console.log(
+        "=========================================================================="
+      );
+      console.log(
+        "MOOVE Location - RAW DATA emitted : IMEI - " + bufferToHexString(data)
+      );
+      console.log(
+        "=========================================================================="
+      );
     }
   });
 });
-
